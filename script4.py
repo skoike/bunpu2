5d2074cc39989d7ec90f39d737339d45804b528b
shin koike <41182422+skoike@users.noreply.github.com>
2024-05-14 20:21:38 +0900

    bunpu2スタート
    ファイル追加

---
+++ script4.py
@@ -0,0 +1,153 @@
+#空間を移動するターゲットをミサイルが迎撃する
+#ミサイルは位置と速度にバラツキを与える。
+#発射時に弾道起動を飛行して、500m以内でターゲットを認識して自律制御にて追尾する
+#
+dlt = 0.1
+test=15+dlt*5*(2+dlt)
+#kakudo=37*np.pi/180#方位1
+kakudo=41*np.pi/180#方位2
+houi= 40*np.pi/180
+vt0=300
+vx0=vt0*np.cos(kakudo)*np.cos(houi)#m/s
+vy0=vt0*np.cos(kakudo)*np.sin(houi)#m/s
+vz0=vt0*np.sin(kakudo)#m/s
+v0=bunpu()
+ms = 4
+vxmin=vx0-5
+vymin=vy0-3
+vzmin=vz0-3
+vxmax=vx0+7
+vymax=vy0+5
+vzmax=vz0+5
+#v0.bunpu_gene([vx0-5,vy0-3,vz0-3],[vx0+7,vy0+5,vz0+5],[vx0,vy0,vz0],[1.0,0.8,0.8],[ms,ms,ms],'v3')
+v0.bunpu_gene([vxmin,vymin,vzmin],[vxmax,vymax,vzmax],[vx0,vy0,vz0],[1.0,0.8,0.8],[ms,ms,ms],'v3')
+breakpoint()
+v0.bunpu_graph('3d v')
+v0.bunpu_file('3d v')
+#x0初期位置のバラツキ
+x0.bunpu_gene([-10,-5,-5],[5,7,5],[0,0,0],[1.0,0.8,0.5],[ms,ms,ms],'x3')
+x0.bunpu_graph('3d x0')
+#幅のない分布
+#a0 = bunpu()
+a0.vector_gene([0,0,-9.8])
+#ターゲットの検出誤差
+#xy.bunpu_gene([-20.0,-20.0,-30.0],[20,20,25],[0,0,0],[4.1,5.1,5.5],[5,5,5],'xtg3')
+xy.bunpu_gene([-20.0,-20.0,-30.0],[20,20,25],[0,0,0],[4.1,5.1,5.5],[ms,ms,ms],'xtg3')
+#検出誤差
+minidist0=[0]
+contact=[]
+ditect = bunpu()
+#ditect.bunpu_gene([-2,-2,-3],[2,2,2],[0,0,0],[0.3,0.2,0.5],[5,5,5],'ditect')
+ditect.bunpu_gene([-2,-2,-3],[2,2,2],[0,0,0],[0.3,0.2,0.5],[ms,ms,ms],'ditect')
+#a = bunpu()
+a = a0
+v = v0
+x = x0
+amin = -50
+amax = 50
+fname='s_amax'
+amx.bunpu_gene([20,30,20],[50,60,50],[30,40,30],[3,2,2],[ms,ms,ms],'a1')
+amx.bunpu_graph(fname)#34
+#ターゲットの初期位置
+xtg0=[3000,1000,2000]
+#xtg0=[3000,1000,3000]
+vtg0=[-100,100,-50]
+#ターゲットの前回値
+lastxy=xy
+#検出距離の初期値
+xrelmin=40000
+acc=a0
+#ターゲット接触判定の距離
+contact0=20
+#ミサイルがターゲットを検出できる距離
+#dists=700
+dists=500.0#制御あり
+#dists=0#制御なし
+#フィードバック制御のゲイン
+gain=200000
+k2 = np.zeros(3)
+cfflag=0
+shw=1
+#forにてループを行うが、スクリプト中にforは一回だけ使用できる。
+#pythonとは異なり、ループ区間はインデントは行わないで、forendでループ終了を指定する
+tm=130
+for t in range(tm):
+if t == 0:
+    y=xy.bunpu_add(xtg0)#分布xyにベクトルxtg0を加算、
+else:
+    vtary = vtg0*dlt
+    y=ypos.bunpu_simu_add(vtary)
+#時系列演算の初期マトリックス作成
+acc,vel,pos,ypos=a0.bunpu_simu_start([v,x,y],dlt,shw)
+#積分演算子、v=∫acc*dt
+v=vel.bunpu_simu_integral([acc],dlt,shw)
+#積分演算子、x=∫x*dt
+x=pos.bunpu_simu_integral([v],dlt,shw)
+
+if t>0:
+    x.timeline=x0.timeline
+
+v0=v
+a0=acc
+a=acc
+xrel=ypos.bunpu_simu_sub(x)
+filename='cont_prb'
+flagx = 0
+#最接近距離の演算、シミュレーション終了時に上記ファイル名で接近距離＊確率分布をもとめる
+minidist=xrel.bunpu_simu_prb(contact0,minidist0,flagx,t,tm,filename)
+minidist0 = minidist
+distx = xrel.bunpu_simu_dist()
+#ターゲットとの距離判定結果
+comp = xrel.bunpu_simu_comp([dists],0)
+#制御開始フラグ
+condc = np.sum(comp)
+if condc>0:
+    distv = vel.bunpu_simu_dist()
+    #ターゲット速度
+    virtualv0 = ypos.bunpu_simu_sub(lasty)
+    virtualv1 = distx/(dlt*distv)
+    virtualv2 = [virtualv1,virtualv1,virtualv1]
+    virtualv = virtualv0.bunpu_simu_prd(virtualv2)
+    #ターゲット仮想位置
+    virtualy = ypos.bunpu_simu_add(virtualv)
+    #仮想ターゲットとの相対位置
+    virtualrelx = virtualy.bunpu_simu_sub(pos)
+    vdistx = virtualrelx.bunpu_simu_dist()
+    #外積による制御量演算
+    op1 = vel.bunpu_simu_outprod(virtualrelx)
+    op2 = op1.bunpu_simu_outprod(vel)
+    k0 = gain/(distv*vdistx**3)
+    k = [k0,k0,k0]#距離が遠いほど小さい
+    
+    if cfflag == 0:
+        cfflag = 1
+        a00 = op2.bunpu_simu_prd(k)
+    else:
+        diffop = op2.bunpu_simu_sub(lastop)
+        diffk = diffop.bunpu_simu_prd(k2)
+        a01 = op2.bunpu_simu_add(diffk)
+        a00 = a01.bunpu_simu_prd(k)#i=92
+    lastop = op2
+    a01 = a00.bunpu_simu_comp_prd(comp)#94
+    limit0 = [200,200,200]
+    a0 = a01.bunpu_simu_limit(limit0)#96
+print(t)#
+cond0=xrelmin>distx#98
+#最接近距離判定
+cond1=~cond0.astype(int)
+cond2=cond0.astype(int)
+xrelmin=xrelmin*cond1+distx*cond2
+gname='timeline_v1'
+x.bunpu_simu_graph(ypos,t,tm,gname,1)
+#ターゲットとの位置分布（20m以内となる逐次確率を求める）
+if t >= 110:
+    x.bunpu_simu_readout()
+    ypos.bunpu_simu_readout()
+    tname='target_'+t
+    x.bunpu_twin_graph(ypos,tname,contact0,1)
+#ターゲットの前回値
+lasty = ypos
+#ループの終わり
+forend
+
+
